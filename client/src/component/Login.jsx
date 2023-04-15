7ca82dedee9bd4457b51ce0323e80340a5560009
░▒▓ Prabhudev Bind ▓▒░ <pbind4545@gmail.com>
2023-04-15 22:43:56 +0530

    save

--- client/src/component/Login.jsx
+++ client/src/component/Login.jsx
@@ -9,13 +9,13 @@ import Navbar from './Navbar'
 const Login = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const history = useNavigate();
+    const navigate = useNavigate();
     useEffect(() => {
         const token = localStorage.getItem('authToken'); // get the token from local storage
         if (token) {
             setIsLoggedIn(true); // set the state to indicate that the user is logged in
             console.log("user Already log in")
-            history('/user');
+            navigate('/user');
         }
     }, []);
     const [fromData, setFromData] = useState({
@@ -44,7 +44,7 @@ const Login = () => {
     return (
         <>
             {isLoggedIn ?
-                history('/dashboard')
+                navigate('/dashboard')
                 : (
                     <div className="relative h-full flex min-h-full justify-center md:px-12 lg:px-0">
                         <div className="relative z-10 h-[100vh] flex flex-1 flex-col bg-white py-10 px-4 shadow-2xl sm:justify-center md:flex-none md:px-28">
